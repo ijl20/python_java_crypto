356e975ba4fad52dfa0c8f97ee8f0ee8e4bb19fa
Ian Lewis <ijl20@cam.ac.uk>
2018-12-24 14:42:54 +0000

    initialization vector now random, prepended to encrypted message

--- java/AESCrypt.java
+++ java/AESCrypt.java
@@ -5,24 +5,38 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 import java.util.Base64;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.lang.*;
+import java.util.Random;
 
 public class AESCrypt {
-    private static final String initVector = "encryptionIntVec";
 
     // String plaintext -> Base64-encoded String ciphertext
     public static String encrypt(String key, String plaintext) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            // Generate a random 16-byte initialization vector
+            byte initVector[] = new byte[16];
+            (new Random()).nextBytes(initVector);
+            IvParameterSpec iv = new IvParameterSpec(initVector);
+
+            // prep the key
             SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
+            // prep the AES Cipher
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
             // Encode the plaintext as array of Bytes
             byte[] cipherbytes = cipher.doFinal(plaintext.getBytes());
 
+            // Build the output message initVector + cipherbytes -> base64
+            byte[] messagebytes = new byte[initVector.length + cipherbytes.length];
+
+            System.arraycopy(initVector, 0, messagebytes, 0, 16);
+            System.arraycopy(cipherbytes, 0, messagebytes, 16, cipherbytes.length);
+
             // Return the cipherbytes as a Base64-encoded string
-            return Base64.getEncoder().encodeToString(cipherbytes);
+            return Base64.getEncoder().encodeToString(messagebytes);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -32,7 +46,13 @@ public class AESCrypt {
     // Base64-encoded String ciphertext -> String plaintext
     public static String decrypt(String key, String ciphertext) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            byte[] cipherbytes = Base64.getDecoder().decode(ciphertext);
+
+            byte[] initVector = Arrays.copyOfRange(cipherbytes,0,16);
+
+            byte[] messagebytes = Arrays.copyOfRange(cipherbytes,16,cipherbytes.length);
+
+            IvParameterSpec iv = new IvParameterSpec(initVector);
             SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
@@ -40,7 +60,7 @@ public class AESCrypt {
 
             // Convert the ciphertext Base64-encoded String back to bytes, and
             // then decrypt
-            byte[] byte_array = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
+            byte[] byte_array = cipher.doFinal(messagebytes);
 
             // Return plaintext as String
             return new String(byte_array, StandardCharsets.UTF_8);
--- python/AES.py
+++ python/AES.py
@@ -1,39 +1,35 @@
 from Crypto.Cipher import AES
 import base64
 import sys
+import os
 
-plaintext = "The quick brown fox jumps over the lâzy dতg";
-#plaintext = "1234567890ABCDEF"
-
-#key = "aesEncryptionKey".encode()  # AES-128 with 16-byte key/iv
-iv =  "encryptionIntVec".encode()
-
+# AES 'pad' byte array to multiple of BLOCK_SIZE bytes
 def pad(byte_array):
     BLOCK_SIZE = 16
     pad_len = BLOCK_SIZE - len(byte_array) % BLOCK_SIZE
-
     return byte_array + (bytes([pad_len]) * pad_len)
 
+# Remove padding at end of byte array
 def unpad(byte_array):
     last_byte = byte_array[-1]
     return byte_array[0:-last_byte]
 
 def encrypt(key, message):
     """
-    Input bytes, return encrypted bytes, using iv and key
+    Input String, return base64 encoded encrypted String
     """
 
     byte_array = message.encode("UTF-8")
 
     padded = pad(byte_array)
 
-    # Note rather than using a fixed initialization vector, we could
-    # generate a random one and prepend that to the encrypted result.
+    # generate a random iv and prepend that to the encrypted result.
     # The recipient then needs to unpack the iv and use it.
-    #iv = Random.new().read(AES.block_size);
+    iv = os.urandom(AES.block_size)
     cipher = AES.new( key.encode("UTF-8"), AES.MODE_CBC, iv )
     encrypted = cipher.encrypt(padded)
-    return base64.b64encode(encrypted).decode("UTF-8")
+    # Note we PREPEND the unencrypted iv to the encrypted message
+    return base64.b64encode(iv+encrypted).decode("UTF-8")
 
 def decrypt(key, message):
     """
@@ -42,9 +38,13 @@ def decrypt(key, message):
 
     byte_array = base64.b64decode(message)
 
+    iv = byte_array[0:16] # extract the 16-byte initialization vector
+
+    messagebytes = byte_array[16:] # encrypted message is the bit after the iv
+
     cipher = AES.new(key.encode("UTF-8"), AES.MODE_CBC, iv )
 
-    decrypted_padded = cipher.decrypt(byte_array)
+    decrypted_padded = cipher.decrypt(messagebytes)
 
     decrypted = unpad(decrypted_padded)
 
